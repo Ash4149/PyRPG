18e66d1a057ad69d4d9dcfd8c6c060bce4a1687c
Ash4149 <152714348+Ash4149@users.noreply.github.com>
2024-10-05 19:25:00 +0200

    Add files via upload

--- entities/simple_entities/EntityTest.py
+++ entities/simple_entities/EntityTest.py
@@ -3,7 +3,5 @@ import entity_class
 
 tile_size = 32
 tiles_move = {0, 2, 3}
-#EntityTestSprite = 'sprites/entities_sprites/simple_entities_sprites/entity_test/neutral.png'
-EntityTestSprite = pygame.Surface((tile_size, tile_size))
+EntityTestSprite = pygame.image.load('sprites/entities_sprites/simple_entities_sprites/entity_test/neutral.png')
 EntityTest = entity_class.SimpleEntity("EntityTest", 17, 6, [[EntityTestSprite]], 0, tiles_move)
-EntityTestSprite.fill((255, 0, 0))
