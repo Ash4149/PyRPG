cead0a99ac2b9589f57ac5d5821e6ea063c6724a
Ash4149 <152714348+Ash4149@users.noreply.github.com>
2024-10-05 19:25:55 +0200

    Add files via upload

---
+++ menu/Equip_menu.py
@@ -0,0 +1,2 @@
+def affich_character(screen, Menu_Character_sprite, group_index):
+    screen.blit(Menu_Character_sprite, (0, 0))
---
+++ menu/principal_menu.py
@@ -0,0 +1,4 @@
+background = 'sprites/menu/back_ground/principal_menu_back_ground.png'
+menu_data = [
+    ["Tag", "Equip", "Pocket", "Skills", "Options"]
+]
